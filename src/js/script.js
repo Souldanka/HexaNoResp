8739da5f06ed172302c75863c289011d0deb9305
Danylo <danillazorenko12@gmail.com>
2020-06-11 03:08:00 +0300

    new

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(".item1").click(function(){
+  $("#carouselExampleIndicators").carousel(0);
+});
+$(".item2").click(function(){
+  $("#carouselExampleIndicators").carousel(1);
+});
+$(".item3").click(function(){
+  $("#carouselExampleIndicators").carousel(2);
+});
+$(".item4").click(function(){
+  $("#carouselExampleIndicators").carousel(3);
+});
